5f938956749f457da93d2057605cf9dd75123e73
woowyl <woowyl@163.com>
2019-03-22 09:11:48 +0800

    模块化

---
+++ PART2/JS进阶/模块化/demo/es6/counter2.js
@@ -0,0 +1,14 @@
+let counter = 1;
+function incCounter() {
+    counter++;
+}
+
+export {
+    counter, 
+    incCounter
+}
+
+// export let counter = 3;
+// export function incCounter() {
+//   counter++;
+// }
--- PART2/JS进阶/模块化/demo/es6/main2.js
+++ PART2/JS进阶/模块化/demo/es6/main2.js
@@ -1,5 +1,12 @@
 
 import Counter from './counter.js';
+import {counter, incCounter} from './counter2.js';
+console.log('1',counter); // 3
+incCounter();
+console.log('122',counter); // 4
+
 Counter.counter = 4;
+Counter.objCounter.num = 9;
 console.log('初始输出:',Counter.counter, Counter.objCounter.num);
 Counter.addCounter();
+console.log('初始输出:',Counter.counter, Counter.objCounter.num);
