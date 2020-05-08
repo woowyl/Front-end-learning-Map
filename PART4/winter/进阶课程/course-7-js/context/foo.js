da78379fe2d148e24fd458456d4c6296c62a5af4
woowyl <woowyl@163.com>
2020-05-08 14:47:39 +0800

    脑图整理

---
+++ PART4/winter/进阶课程/course-7-js/context/foo.js
@@ -0,0 +1,5 @@
+function foo() {
+    console.log("i in foo", i)
+}
+
+export default foo;
---
+++ PART4/winter/进阶课程/course-7-js/context/main.js
@@ -0,0 +1,8 @@
+import {foo} from 'foo.js';
+var i = 0
+console.log(i,"in first line");
+i ++
+foo();
+console.log(i,"in second line");
+i++
+console.log(i,"in third line");
