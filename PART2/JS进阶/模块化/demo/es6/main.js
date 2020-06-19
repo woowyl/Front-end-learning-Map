7881b35b17f89618965d70ada8d82382a6703301
woowyl <woowyl@163.com>
2020-06-19 19:58:42 +0800

    es6

--- PART2/JS进阶/模块化/demo/es6/main.js
+++ PART2/JS进阶/模块化/demo/es6/main.js
@@ -1,5 +1,9 @@
 
 import Counter from './counter.js';
+// import {counter, objCounter, addCounter}  from './counter.js';
 
 console.log('main第一次输入出:',Counter.counter, Counter.objCounter.num);
 Counter.addCounter();
+
+// console.log('main第一次输入出:',counter, objCounter.num);
+// addCounter();
--- PART2/JS进阶/模块化/demo/es6/main2.js
+++ PART2/JS进阶/模块化/demo/es6/main2.js
@@ -1,9 +1,12 @@
 
+// import {counter, addCounter} from './counter.js';
+// import * as Counter from './counter.js';
+// import {counter, objCounter, addCounter} from './counter.js';
 import Counter from './counter.js';
-import {counter, incCounter} from './counter2.js';
-console.log('1',counter); // 3
-incCounter();
-console.log('122',counter); // 4
+// import {counter, incCounter} from './counter2.js';
+// console.log('1',counter); // 3
+// incCounter();
+// console.log('122',counter); // 4
 
 Counter.counter = 4;
 Counter.objCounter.num = 9;
@@ -11,3 +14,12 @@ console.log('初始输出:',Counter.counter, Counter.objCounter.num);
 Counter.addCounter();
 console.log('初始输出:',Counter.counter, Counter.objCounter.num);
 Counter.counter = 14;
+
+// counter = 4;
+// objCounter.num = 9;
+// console.log('初始输出:',counter, objCounter.num);
+// addCounter();
+// console.log('初始输出:',counter, objCounter.num);
+// counter = 14;
+
+// console.log('初始输出:',counter)
