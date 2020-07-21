4de9872e072b54cdb3d2d3a9510bf2575c20a61a
woowyl <woowyl@163.com>
2020-07-21 18:10:06 +0800

    异步编程

--- PART4/winter/进阶课程/course-12-编程练习-promise/red_yellow_green/index.js
+++ PART4/winter/进阶课程/course-12-编程练习-promise/red_yellow_green/index.js
@@ -1,6 +1,4 @@
 
-    
-    console.log("22");
     let greenLight = document.getElementsByClassName("green");
     let redLight = document.getElementsByClassName("red");
     let yellowLight = document.getElementsByClassName("yellow");
@@ -27,7 +25,7 @@
         yellowLight[0].classList.add("lighted");
     }
 
-    // 方法1
+    // 方法 settime时序
     function go1() {
         green();
         setTimeout(() => {
@@ -41,6 +39,7 @@
         }, 3000);
     }
 
+    // 方法2  settimeout 嵌套
     function go2() {
         green();
         setTimeout(() => {
@@ -53,3 +52,135 @@
             }, 1000);
         }, 1000);
     }
+
+    // 方法3 promise  链式表达
+    function go3() {
+        green();
+        sleep(1000).then(() => {
+            yellow();
+            return sleep(1000);
+        }).then(() => {
+            red();
+            return sleep(1000);
+        }).then(go3)
+    }
+
+    function sleep(t) {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve();
+            }, t);
+        })
+    }
+
+    // 方法4 await async + 递归异步编程
+    async function go4() {
+        green();
+        await sleep(2000);
+        yellow();
+        await sleep(1000);
+        red();
+        await sleep(2000);
+        go4();
+    }
+
+    // 方法5 await async + while 异步编程
+    async function go5() {
+        while (true) {
+            green();
+            await sleep(2000);
+            yellow();
+            await sleep(1000);
+            red();
+            await sleep(2000);
+        }
+    }
+
+    // 手动控制
+    function happen(ele, event) {
+        return new Promise((resolve, reject) => {
+            ele.addEventListener(event, resolve, {once: true})
+        })
+    }
+
+    async function go6() {
+        while (true) {
+            green();
+            await happen(document.getElementById("next"), "click");
+            yellow();
+            await happen(document.getElementById("next"), "click");
+            red();
+            await happen(document.getElementById("next"), "click");
+        }
+    }
+
+    // generator 版本
+    
+    function* go7() {
+        while (true) {
+            green();
+            yield sleep(2000);
+            yellow();
+            yield sleep(1000);
+            red();
+            yield sleep(2000);
+        }
+    }
+
+    function co(iterator) {
+        let {value, done} = iterator.next();
+        if (done) return;
+        if (value instanceof Promise) {
+            value.then(() => {
+                co(iterator)
+            })
+        }
+    }
+
+    //co(go7())
+
+    // generator 改进版本
+
+    function* go8() {
+        while (true) {
+            green();
+            yield sleep(2000);
+            yellow();
+            yield sleep(1000);
+            red();
+            yield sleep(2000);
+        }
+    }
+
+    function run(iterator) {
+        let {value, done} = iterator.next();
+        if (done) return;
+        if (value instanceof Promise) {
+            value.then(() => {
+                run(iterator)
+            })
+        }
+    }
+
+    function co2(generator) {
+        return function() {
+            return run(generator())
+        }
+    }
+
+    go8 = co2(go8)
+
+    // 无线循环的例子
+    async function* g() {
+        let i = 0;
+        while (true) {
+            await sleep(1000);
+            yield i++;
+        }
+    }
+
+    //执行一下js
+
+    // for await(let v of g()) {
+    //     console.log(v);
+    // }
